91fc762850c4cd8d4c5dda61db3c4bb4716e2aed
sanjugahara <junyang7749@gmail.com>
2017-08-17 21:03:59 -0700

    login commit

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -37,7 +37,14 @@ public class RecommendItem extends HttpServlet {
    */
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    String userId = request.getParameter("user_id");
+	  HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+
+		String userId = session.getAttribute("user").toString();
+    //String userId = request.getParameter("user_id");
     double lat = Double.parseDouble(request.getParameter("lat"));
     double lon = Double.parseDouble(request.getParameter("lon"));
     Recommendation recommendation = new GeoRecommendation();
@@ -54,6 +61,7 @@ public class RecommendItem extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     // TODO Auto-generated method stub
+
     doGet(request, response);
   }
 }
